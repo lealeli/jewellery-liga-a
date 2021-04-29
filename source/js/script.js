42451d8506eed8a05d006c3b63f9ec0ba15fb273
Elena <elenamitchenko@yandex.ru>
2021-04-30 00:28:38 +0300

    Blog FAQ done

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,30 @@
 'use strict';
 
-var button = document.querySelector('.page-header__toggle');
-var menu = document.querySelector('.page-header__wrapper');
-var subMenu = document.querySelector('.main-nav');
+/* global Flickity */
+(function () {
+  var button = document.querySelector('.page-header__toggle');
+  var menu = document.querySelector('.page-header__wrapper');
+  var subMenu = document.querySelector('.main-nav');
 
-function clickMenu() {
-  button.classList.toggle('page-header__toggle--open');
-  subMenu.classList.toggle('main-nav--open');
-}
+  function clickMenu() {
+    button.classList.toggle('page-header__toggle--open');
+    subMenu.classList.toggle('main-nav--open');
+  }
 
-if (button && subMenu && menu) {
-  button.classList.remove('page-header__toggle--nojs');
-  subMenu.classList.remove('main-nav--nojs');
-  menu.classList.remove('page-header__wrapper--nojs');
-  button.addEventListener('click', clickMenu);
-}
+  if (button && subMenu && menu) {
+    button.classList.remove('page-header__toggle--nojs');
+    subMenu.classList.remove('main-nav--nojs');
+    menu.classList.remove('page-header__wrapper--nojs');
+    button.addEventListener('click', clickMenu);
+  }
+
+  var elem = document.querySelector('.qwe');
+  var flkty = new Flickity(elem, {
+    // options
+    contain: true,
+    groupCells: true
+  });
+  console.log(flkty);
+
+
+})();
