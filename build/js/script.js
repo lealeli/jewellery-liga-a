5bf00c55a2bfb0e54e5cff8fd11defc73ec7b6d3
Elena <elenamitchenko@yandex.ru>
2021-05-04 01:32:57 +0300

    PerfectPixel main

--- build/js/script.js
+++ build/js/script.js
@@ -18,13 +18,37 @@
     button.addEventListener('click', clickMenu);
   }
 
-  var elem = document.querySelector('.qwe');
-  var flkty = new Flickity(elem, {
+  var sldElem = document.querySelector('.new-goods__list');
+  var sldPagination = document.querySelector('.new-goods__pagination');
+  var flkty = new Flickity(sldElem, {
     // options
     contain: true,
     groupCells: true
   });
-  console.log(flkty);
+  var slideHandle = function () {
+    var text = (flkty.selectedIndex + 1) + ' of ' + flkty.slides.length;
+    sldPagination.innerHTML = text;
+  };
+  slideHandle();
+  flkty.on('select', slideHandle);
+
+
+  var toggleFooter = document.querySelectorAll('.faq__item');
+  toggleFooter.forEach(function (el, idx) {
+    el.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      toggleFooter.forEach(function (el2, idx2) {
+        if (idx === idx2) {
+          el2.classList.toggle('faq__item--open');
+        } else {
+          el2.classList.remove('faq__item--open');
+        }
+      });
+    });
+
+    el.classList.remove('faq__item--open');
+  });
 
 
 })();
