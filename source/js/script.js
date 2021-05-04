12eecb2612f5e0aef05a922cf03f3af28c2b3436
Elena <elenamitchenko@yandex.ru>
2021-05-04 22:57:50 +0300

    catalog done

--- source/js/script.js
+++ source/js/script.js
@@ -19,26 +19,28 @@
   }
 
   var sldElem = document.querySelector('.new-goods__list');
-  var sldPagination = document.querySelector('.new-goods__pagination');
-  var flkty = new Flickity(sldElem, {
-    // options
-    contain: true,
-    groupCells: true
-  });
-  var slideHandle = function () {
-    var text = (flkty.selectedIndex + 1) + ' of ' + flkty.slides.length;
-    sldPagination.innerHTML = text;
-  };
-  slideHandle();
-  flkty.on('select', slideHandle);
+  if (sldElem) {
+    var sldPagination = document.querySelector('.new-goods__pagination');
+    var flkty = new Flickity(sldElem, {
+      // options
+      contain: true,
+      groupCells: true
+    });
+    var slideHandle = function () {
+      var text = (flkty.selectedIndex + 1) + ' of ' + flkty.slides.length;
+      sldPagination.innerHTML = text;
+    };
+    slideHandle();
+    flkty.on('select', slideHandle);
+  }
 
 
-  var toggleFooter = document.querySelectorAll('.faq__item');
-  toggleFooter.forEach(function (el, idx) {
+  var toggleFaq = document.querySelectorAll('.faq__item');
+  toggleFaq.forEach(function (el, idx) {
     el.addEventListener('click', function (e) {
       e.preventDefault();
 
-      toggleFooter.forEach(function (el2, idx2) {
+      toggleFaq.forEach(function (el2, idx2) {
         if (idx === idx2) {
           el2.classList.toggle('faq__item--open');
         } else {
@@ -49,6 +51,34 @@
 
     el.classList.remove('faq__item--open');
   });
+  var buttonFilter = document.querySelector('.catalog__filter-button');
+  var filter = document.querySelector('.filter');
+  var filterButtonClose = document.querySelector('.filter_button-close');
 
+  buttonFilter.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    filter.classList.toggle('filter--open');
+  });
+
+  filterButtonClose.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    filter.classList.toggle('filter--open');
+  });
+
+  filter.classList.remove('filter--open');
+
+  var toggleFilter = document.querySelectorAll('.filter__item');
+
+  toggleFilter.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      el.classList.toggle('filter__item--open');
+    });
+
+    el.classList.remove('filter__item--open');
+  });
 
 })();
